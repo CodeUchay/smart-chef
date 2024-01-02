46450c3ff457cb744a1b137ae0669aeac8fbcce6
Uche Odinamba <uchay2x@icloud.com>
2024-01-02 15:22:25 +0100

    Initial comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import DateScanner from './components/DateScanner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto  p-4 bg-slate-600 h-screen flex items-center justify-center">
+      <DateScanner />
     </div>
   );
 }
---
+++ src/components/DateScanner.jsx
@@ -0,0 +1,110 @@
+// src/components/DateScanner.js
+import React, { useState, useEffect } from 'react';
+import Webcam from 'react-webcam';
+import { createWorker } from 'tesseract.js';
+
+const DateScanner = () => {
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [scannedDate, setScannedDate] = useState('');
+  const [textExtracted, setTextExtracted] = useState('');
+
+  // camera front or back
+  const [cameraMode, setCameraMode] = useState('user');
+  const videoConstraints = {
+    facingMode: cameraMode
+  };
+
+  function toggleCamera(){
+    if (cameraMode !== 'user' ){
+      setCameraMode('user')
+    }
+    else {
+      setCameraMode({ exact: "environment" })
+    }
+  }
+  const webcamRef = React.createRef();
+  const worker = createWorker({
+    logger: m => console.log(m)
+  });
+  const captureImage = async () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCapturedImage(imageSrc);
+  
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const webpImage = new Image();
+    
+    webpImage.onload = () => {
+      canvas.width = webpImage.width;
+      canvas.height = webpImage.height;
+      context.drawImage(webpImage, 0, 0, webpImage.width, webpImage.height);
+      
+      // Convert the canvas content to a PNG Data URL
+      const pngDataUrl = canvas.toDataURL('image/png');
+  
+      // Pass the PNG Data URL to Tesseract for recognition
+      recognizeTextInImage(pngDataUrl);
+    };
+  
+    webpImage.src = imageSrc;
+  };
+  
+  const recognizeTextInImage = async (imageSrc) => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+  
+    const { data: { text } } = await worker.recognize(imageSrc);
+    console.log(text);
+    setTextExtracted(text);
+    const dateRegex = /\d{2}\/\d{2}\/\d{4}/; // Customize this regex for your date format
+
+    const extractedDate = text.match(dateRegex);
+    setScannedDate(extractedDate);
+  
+    await worker.terminate();
+  };
+  
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setCapturedImage(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+  return (
+    <div className='bg-slate-700 rounded-xl '>
+      <h1 className='text-center text-white font-sans text-xl p-3'>Date Scanner</h1>
+      {capturedImage ? (
+        <>
+          <img src={capturedImage} className='h-80 w-100' alt="Captured" />
+          {textExtracted && <p className='text-white p-4'>Extracted Text: {textExtracted}
+          </p>}
+          {scannedDate && <p className='text-white p-4'>Extracted Date: {scannedDate}
+          </p>}
+        </>
+      ) : (
+        <div>
+          <Webcam ref={webcamRef} videoConstraints={videoConstraints} className='border w-72 h-100 lg:w-120 lg:h-180 m-3 border-slate-500 rounded-xl'/>
+          <div> <div className="mb-4 flex flex-col">
+           <div className='flex flex-row'> 
+          <button onClick={captureImage} className='m-2 mx-auto bg-slate-200 rounded px-3 py-2'>Capture Image</button>
+          <button onClick={toggleCamera} className='m-2 mx-auto bg-slate-200 rounded px-2 py-2'>Turn Camera</button>
+          </div> <span className='text-white text-center font-semibold'>OR</span>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          className="m-2 bg-slate-200 rounded"
+        />
+      </div></div>
+          </div>
+      )}
+    </div>
+  );
+};
+
+export default DateScanner;
